9099816aee1283c6eedea155bedbd3c65fa53e9a
Rishab Alagharu <rishabsaia@gmail.com>
2025-03-04 21:30:20 -0500

    hi

---
+++ reinforcement/rl-snake-pygame/model.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=input_size, out_features=hidden_size),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_size, out_features=output_size),
+        )
+
+    def forward(self, x):
+        return self.linear(x)
+
+    def save(self, file_name="rl_qnet.pt"):
+        model_folder_path = "./model"
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+    def load(self, file_name="rl_qnet.pt"):
+        model_folder_path = "./model"
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        if os.path.exists(file_name):
+            self.load_state_dict(torch.load(file_name))
+
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.AdamW(params=model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
+
+        if len(state.shape) == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, dim=0)
+            next_state = torch.unsqueeze(next_state, dim=0)
+            action = torch.unsqueeze(action, dim=0)
+            reward = torch.unsqueeze(reward, dim=0)
+            done = (done,)
+
+        # 1. Predicted Q values with the current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(
+                    self.model(next_state[idx])
+                )
+
+            target[idx][torch.argmax(action).item()] = Q_new
+
+        # 2. Q_new = r + \gamma * max(next_predicted Q value) -> Only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        loss = self.criterion(target, pred)
+
+        self.optimizer.zero_grad()
+
+        loss.backward()
+
+        self.optimizer.step()
